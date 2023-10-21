d4eeaa3d9f1877e0b474e7ff96d22d282c2b52e7
Maggie Keller <magdalene.keller@selu.edu>
2023-10-21 02:28:03 +0000

    Histogram Function

---
+++ keller.pkg/R/Histogram.R
@@ -0,0 +1,3 @@
+This function will make histogram plots. This is where I will need to insert
+instructions on how to do the count code since we realized writing a function
+for that is not feasible.
