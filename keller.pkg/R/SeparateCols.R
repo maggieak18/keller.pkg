8772625a8dfc5d44a35beb987d0ae9723c3fe6a8
Maggie Keller <magdalene.keller@selu.edu>
2023-10-24 20:39:20 +0000

    added head and function code

--- keller.pkg/R/SeparateCols.R
+++ keller.pkg/R/SeparateCols.R
@@ -1,1 +1,23 @@
-This function will be able to separate columns in order to look at different experimental groups if desired.
+#'Separate experimental group columns
+#'
+#'Uses the experimental group column to create new columns so sensory manipulations
+#'can be isolated if desired
+#'
+#'@param data the data set being utilized
+#'@param column the column that is being split
+#'@param newname1 the desired name of the column created from the characters to the left of the delimiter aka _
+#'@param newname2 the desired name of the column created from the characters to the right of the delimiter aka _
+#'
+#'@export
+
+
+separate_cols <- function(data, column, newname1, newname2){
+  final <- separate_wider_delim(data = data, cols = column, delim = " ", names = c(newname1, newname2))
+    return(final)
+}
+
+
+example
+separate_cols(data = frog_data, column = "experimental_group", newname1 = "visual", newname2 = "lateral_line")
+
+
